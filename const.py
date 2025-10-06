32a328f7f57abb9e2bc6edf17b276fb3b6dbb7fc
noccu <37862870+noccu@users.noreply.github.com>
2025-10-07 00:08:44 +0200

    Simplify diff gen args and allow target selection

---
+++ const.py
@@ -0,0 +1,7 @@
+from os import environ
+from pathlib import Path
+
+GAME_ROOT = Path(environ["LOCALAPPDATA"], "..", "LocalLow", "Cygames", "umamusume").resolve()
+GAME_ASSET_ROOT = GAME_ROOT.joinpath("dat")
+GAME_META_FILE = GAME_ROOT.joinpath("meta")
+GAME_MASTER_FILE = GAME_ROOT.joinpath("master", "master.mdb")
