f6ead1295a67da05097b9300c15623a4f796c841
noccu <37862870+noccu@users.noreply.github.com>
2025-10-07 15:24:42 +0200

    Adjust atlas meta gen

--- atlas_meta_gen.py
+++ atlas_meta_gen.py
@@ -1,23 +1,23 @@
 from pathlib import Path
 import json
 from sys import argv
-import sqlite3
 from meta_db_lib import MetaDb
+import const
+
 
 def main():
-    (windows_meta, android_meta, atlas_dir) = argv[1:]
+    (android_meta, atlas_dir) = argv[1:]
 
-    windows_meta = MetaDb(windows_meta)
+    windows_meta = MetaDb(const.GAME_META_FILE)
     android_meta = MetaDb(android_meta)
 
     for child in Path(atlas_dir).iterdir():
         if child.is_dir():
             bundle_name = "atlas/{0}/{0}".format(child.name)
-            windows_hash = windows_meta.get_asset_hash_and_key(bundle_name)
-            android_hash = android_meta.get_asset_hash_and_key(bundle_name)
+            windows_hash, _ = windows_meta.get_asset_hash_and_key(bundle_name)
+            android_hash, _ = android_meta.get_asset_hash_and_key(bundle_name)
 
-            print(bundle_name)
-
+            print(f"Processing: {bundle_name}")
             if not windows_hash and not android_hash:
                 print("[Warn] Asset not found, skipping")
                 continue
@@ -25,16 +25,17 @@ def main():
             meta = {}
 
             if windows_hash:
-                meta["windows"] = {'bundle_name': windows_hash}
+                meta["windows"] = {"bundle_name": windows_hash}
             else:
                 print("[Warn] Asset not found for Windows")
 
             if android_hash:
-                meta["android"] = {'bundle_name': android_hash}
+                meta["android"] = {"bundle_name": android_hash}
             else:
                 print("[Warn] Asset not found for Android")
 
-            with open(child / (child.name + ".json"), "w", encoding="utf-8", newline='\n') as f:
-                json.dump(meta, f, ensure_ascii=False, indent=4)
+            with open(child / f"{child.name}.json", "w", encoding="utf-8", newline="\n") as f:
+                json.dump(meta, f, ensure_ascii=False, indent=2)
+
 
 main()
