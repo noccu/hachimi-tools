853f077fb11c2aea2bf14f693816d43ea97e4749
noccu <37862870+noccu@users.noreply.github.com>
2025-10-07 02:49:28 +0200

    Refactor bundle dl
    Separate out an importable download func.

--- bundle_dl.py
+++ bundle_dl.py
@@ -3,29 +3,38 @@ import sqlite3
 from pathlib import Path
 import requests
 from meta_db_lib import MetaDb
+import const
 
-def main():
-    (output_dir, meta_path, bundle_name) = argv[1:]
 
-    meta = MetaDb(meta_path)
-
-    asset_hash = meta.get_asset_hash_and_key(bundle_name)
-    print(asset_hash)
-    if asset_hash == None:
-        return
-
-    path = output_dir / Path(bundle_name)
-    path = path.with_name(path.name + "_" + asset_hash)
-    path.parent.mkdir(parents=True, exist_ok=True)
-
+def download(meta: MetaDb, asset_hash) -> bytes | int:
     response = requests.get(meta.get_asset_bundle_url(asset_hash))
     status = response.status_code
 
     if status == 200:
-        with open(path, "wb") as file:
-            file.write(response.content)
-        print("Done")
+        return response.content
     else:
-        print("Got status code {}".format(status))
+        return status
 
-main()
+
+def main():
+    (output_dir, bundle_name) = argv[1:]
+    meta = MetaDb(const.GAME_META_FILE)
+    asset_hash, _ = meta.get_asset_hash_and_key(bundle_name)
+    if asset_hash == None:
+        return
+    print(f"Found hash: {asset_hash}")
+
+    path = Path(output_dir, asset_hash)
+    path = path.with_name(f"{path.name}_{asset_hash}")
+    path.parent.mkdir(parents=True, exist_ok=True)
+    asset_data = download(meta, asset_hash)
+    if isinstance(asset_data, int):
+        print(f"Failed download. Status code: {asset_data}")
+        return
+
+    path.write_bytes(asset_data)
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
