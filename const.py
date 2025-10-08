ac2d39477bb217d0d616ac081e36af3f1c038dd5
noccu <37862870+noccu@users.noreply.github.com>
2025-10-08 19:19:27 +0200

    Support setting game data dir using env var

--- const.py
+++ const.py
@@ -1,7 +1,12 @@
 from os import environ
 from pathlib import Path
 
-GAME_ROOT = Path(environ["LOCALAPPDATA"], "..", "LocalLow", "Cygames", "umamusume").resolve()
+GAME_ROOT = environ.get("UMA_DATA_DIR")
+if GAME_ROOT:
+    GAME_ROOT = Path(GAME_ROOT)
+else:
+    GAME_ROOT = Path(environ["LOCALAPPDATA"], "..", "LocalLow", "Cygames", "umamusume").resolve()
+
 GAME_ASSET_ROOT = GAME_ROOT.joinpath("dat")
 GAME_META_FILE = GAME_ROOT.joinpath("meta")
 GAME_MASTER_FILE = GAME_ROOT.joinpath("master", "master.mdb")
