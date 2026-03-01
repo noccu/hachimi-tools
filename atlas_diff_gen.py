377ba1065c06b3215c85d7a06f6bfbe85b66c908
noccu <37862870+noccu@users.noreply.github.com>
2026-03-01 19:45:22 +0100

    Simplify atlas_diff_gen

--- atlas_diff_gen.py
+++ atlas_diff_gen.py
@@ -2,7 +2,6 @@ from pathlib import Path
 from unitypy_utils import *
 from sys import argv
 from const import GAME_META_FILE
-import requests
 import UnityPy
 from PIL import Image
 from meta_db_lib import MetaDb
@@ -10,6 +9,7 @@ from png_diff_lib import png_diff
 from utils import load_ignore_list, get_ld_assets_root
 from decrypt import decrypt_asset_bundle
 import const
+from bundle_utils import get_bundle_data
 
 def main():
     (atlas_dir, *target_dirs) = argv[1:]
@@ -39,27 +39,21 @@ def main():
                 if not bundle_hash:
                     print("[Warn] Asset not found, skipping")
                     continue
-                bundle_path = meta.get_asset_bundle_path(bundle_hash)
-                bundle_url = meta.get_asset_bundle_url(bundle_hash)
 
-                if bundle_path.is_file():
-                    bundle_data = bundle_path.read_bytes()
-                    status = 200
-                else:
-                    bundle_res = requests.get(bundle_url)
-                    bundle_data = bundle_res.content
-                    status = bundle_res.status_code
-                if status == 200:
-                    env = UnityPy.load(decrypt_asset_bundle(bundle_data, bundle_key))
-                    texture = find_first_texture_2d(env)
-                    if not texture:
-                        print("[Error] Texture not found (invalid or failed to load asset bundle)")
-                        continue
+                try:
+                    bundle_data = get_bundle_data(meta, bundle_hash)
+                except FileNotFoundError as e:
+                    print(f"Bundle {bundle_hash} not found.\n{e}")
+                    continue
 
-                    diff_img = png_diff(texture.image, rep_img)
-                    diff_path = child / f"{child.name}.diff.png"
-                    diff_path.write_bytes(diff_img)
-                else:
-                    print("Got status code {}".format(status))
+                env = UnityPy.load(decrypt_asset_bundle(bundle_data, bundle_key))
+                texture = find_first_texture_2d(env)
+                if not texture:
+                    print("[Error] Texture not found (invalid or failed to load asset bundle)")
+                    continue
+
+                diff_img = png_diff(texture.image, rep_img)
+                diff_path = child / f"{child.name}.diff.png"
+                diff_path.write_bytes(diff_img)
 
 main()
