48953d3084cf61cdfe2a3ec97de0ca5324ee0548
noccu <37862870+noccu@users.noreply.github.com>
2026-03-06 12:06:46 +0100

    Modernize mattegen

--- mattegen.py
+++ mattegen.py
@@ -1,17 +1,36 @@
+from pathlib import Path
+from sys import argv
+
 import UnityPy
-from UnityPy.classes import Sprite
 from PIL import Image, ImageDraw
+from UnityPy.classes import Sprite
+
+import const
+import utils
+from bundle_utils import get_bundle_data
+from decrypt import decrypt_asset_bundle
+from meta_db_lib import MetaDb
 from unitypy_utils import find_first_texture_2d, rect_to_coords
 
+
 def main():
-    bundle_path = input("Enter bundle path: ").strip()
-    env = UnityPy.load(bundle_path)
+    (out_dir, atlas_name) = argv[1:]
+    if out_dir == const.USE_TL_SRC_PATH:
+        out_dir = utils.get_ld_assets_root("atlas")
+    else:
+        out_dir = Path(out_dir)
+        out_dir.mkdir(parents=True, exist_ok=True)
+    meta = MetaDb(const.GAME_META_FILE)
+
+    hash, key = meta.get_asset_hash_and_key(f"atlas/{atlas_name}/{atlas_name}_tex")
+    bundle_data = decrypt_asset_bundle(get_bundle_data(meta, hash), key)
+    env = UnityPy.load(bundle_data)
 
     texture = find_first_texture_2d(env)
     if not texture:
         print("[Error] Texture not found (invalid or failed to load asset bundle)")
         return
-    
+
     width = texture.m_Width
     height = texture.m_Height
     im = Image.new("RGBA", (width, height), None)
@@ -22,8 +41,9 @@ def main():
             coords = rect_to_coords(data.m_Rect, height)
             im_draw.rectangle(coords, "#ff00ff")
 
-    out_path = bundle_path + "_matte.png"
+    out_path = out_dir / f"{atlas_name}_matte.png"
     im.save(out_path, "PNG", compress_level=9)
+    print(f"Wrote: {out_path}")
 
 
 main()
