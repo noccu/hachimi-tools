a687dfac7f71dacfb944d014c71ff049917f5090
noccu <37862870+noccu@users.noreply.github.com>
2026-03-07 14:30:57 +0100

    Allow custom key in meta decryption script
    NOTE: This only applies to the individual script, not generally.

--- meta_decrypt.py
+++ meta_decrypt.py
@@ -15,7 +15,7 @@ def parse_args():
     parser.add_argument(
         "dst", nargs="?", help="Path to save decrypted DB (default: decrypted_metas/<src name>_<iso date>.sqlite)"
     )
-    # parser.add_argument("--key", dest="key_hex", help="Hex key (default: hardcoded key)")
+    parser.add_argument("--key", "-k", help="Hex key (default: hardcoded key)")
     # parser.add_argument(
     #     "--cipher", dest="cipher", type=int, default=3, help="sqlite3mc cipher index to set (default: 3)"
     # )
@@ -31,7 +31,7 @@ def main():
         args.dst = DECRYPTED_METAS_DIR / f"{args.src.stem}_{date.today().isoformat()}.sqlite"
 
     print(f"Decrypting {args.src} to {args.dst}")
-    meta = MetaDb(args.src)
+    meta = MetaDb(args.src, key=args.key)
     meta.cur.close()  # Old class impl detail gets in the way.
     try:
         meta.db.execute(f"VACUUM INTO 'file:{str(args.dst)}?key='")
