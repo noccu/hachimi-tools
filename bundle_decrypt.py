8f21afac72052bdf33291aa2878832b038ac6199
noccu <37862870+noccu@users.noreply.github.com>
2026-03-12 21:01:29 +0100

    Allow bundle decrypt to download bundles

--- bundle_decrypt.py
+++ bundle_decrypt.py
@@ -1,21 +1,25 @@
-from decrypt import decrypt_asset_bundle
-from sys import argv
-from meta_db_lib import MetaDb
 from pathlib import Path
+from sys import argv
+
 import const
+from bundle_utils import get_bundle_data
+from decrypt import decrypt_asset_bundle
+from meta_db_lib import MetaDb
 
 
-def decrypt(bundle_path: Path, key: int, name: str):
-    if not bundle_path.is_file():
-        print(f"Bundle for {name} doesn't exist at {bundle_path}")
-        return
+def decrypt(bundle_data: Path | bytes, key: int, name: str):
+    if isinstance(bundle_data, Path):
+        try:
+            bundle_data = bundle_data.read_bytes()
+        except FileNotFoundError:
+            print(f"Bundle for {name} doesn't exist at {bundle_data}")
+            return
 
     out_path = const.DECRYPTED_BUNDLES_DIR / Path(name).name
     if out_path.is_file():
         print(f"Skipping existing: {name}")
         return
     out_path.parent.mkdir(exist_ok=True)
-    bundle_data = bundle_path.read_bytes()
     out_path.write_bytes(decrypt_asset_bundle(bundle_data, key))
     print(f"Wrote {out_path.name}")
 
@@ -30,14 +34,12 @@ def main():
     else:
         db = MetaDb(const.GAME_META_FILE)
         # Path
-        if any(s in asset_hash_or_path for s in ("/", "_")):
+        if any(s in asset_hash_or_path for s in ("/", "_", "%")):
             query = f"SELECT n, h, e FROM a WHERE n LIKE '%{asset_hash_or_path}%'"
-            asset_meta = db.cur.execute(query).fetchall()
-            if asset_meta:
-                for asset_path, asset_hash, asset_key in asset_meta:
-                    bundle_path = db.get_asset_bundle_path(asset_hash)
-                    decrypt(bundle_path, asset_key, asset_path)
-            else:
+            asset_meta = db.cur.execute(query)
+            for asset_path, asset_hash, asset_key in asset_meta:
+                decrypt(get_bundle_data(db, asset_hash), asset_key, asset_path)
+            if "asset_path" not in locals():
                 print(f"No bundles found: {asset_hash_or_path}")
         # Hash
         else:
@@ -45,8 +47,7 @@ def main():
             asset_meta = db.cur.execute(query).fetchone()
             if asset_meta:
                 asset_path, asset_key = asset_meta
-                bundle_path = db.get_asset_bundle_path(asset_hash_or_path)
-                decrypt(bundle_path, asset_key, asset_path)
+                decrypt(get_bundle_data(db, asset_hash_or_path), asset_key, asset_path)
             else:
                 print(f"Bundle not found: {asset_hash_or_path}")
         db.close()
